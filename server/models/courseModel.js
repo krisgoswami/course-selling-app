4d81583c26a09bb9e912ef2815b3251474247259
Krishanu <krishgoswami15@gmail.com>
2023-09-26 22:10:50 +0530

    updated model

--- server/models/courseModel.js
+++ server/models/courseModel.js
@@ -1,11 +1,34 @@
 import mongoose from "mongoose";
 
+// const adminSchema = new mongoose.Schema({
+//     title: String,
+//     description: String,
+//     price: Number,
+//     imageLink: String,
+//     published: Boolean,
+// });
+
 const adminSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    imageLink: String,
-    published: Boolean,
-});
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "description is required"],
+    },
+    price: {
+        type: Number,
+        required: [true, "price is required"],
+    },
+    imageLink: {
+        type: String,
+        required: [true, "imageLink is required"],
+    },
+    published: {
+        type: Boolean,
+        required: [true, "published is required"],
+    },
+})
 
 export const Course = mongoose.model("Course", adminSchema);

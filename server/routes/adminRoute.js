2278b9f058bc81dfebb39f753ab0ba9d88147b8a
Krishanu <krishgoswami15@gmail.com>
2023-09-26 22:11:00 +0530

    added get by id route

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authenticateJwt } from "../utils/jwtAuth.js";
-import { createAdmin, adminLogin, createCourse, updateCourse, deleteCourse, getCourses } from '../controllers/adminController.js'
+import { createAdmin, adminLogin, createCourse, updateCourse, deleteCourse, getCourses, getCourse } from '../controllers/adminController.js'
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post('/signup', createAdmin);
 router.post('/login', adminLogin);
 router.post('/createCourse', authenticateJwt, createCourse);
 router.get('/courses', authenticateJwt, getCourses);
+router.get('/course/:id', authenticateJwt, getCourse);
 router.put('/updateCourse/:id', authenticateJwt, updateCourse);
 router.delete('/deleteCourse/:id', authenticateJwt, deleteCourse);
 

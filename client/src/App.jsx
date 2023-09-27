76068bf8e635e4f9287b1e1abce2bb993129e4f9
Krishanu <krishgoswami15@gmail.com>
2023-09-27 15:09:31 +0530

    moved routes into another file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AppBar from './components/AppBar';
-import Signup from './pages/Signup';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import CreateCourse from './pages/CreateCourse';
-import EditCourse from './pages/EditCourse';
-import Courses from './pages/Courses';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import NavRoutes from './NavRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Provider store={store}>
-        <Router>
-          <AppBar />
-          <Routes>
-            <Route path='/landing' element={<Landing />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/create-course' element={<CreateCourse />} />
-            <Route path='/edit-course/:id' element={<EditCourse />} />
-            <Route path='/courses' element={<Courses />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <NavRoutes />
     </>
   )
 }
---
+++ client/src/NavRoutes.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppBar from './components/AppBar';
+import Signup from './pages/Signup';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import CreateCourse from './pages/CreateCourse';
+import EditCourse from './pages/EditCourse';
+import Courses from './pages/Courses';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+import React from 'react'
+
+const NavRoutes = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <AppBar />
+                <Routes>
+                    <Route path='/landing' element={<Landing />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/create-course' element={<CreateCourse />} />
+                    <Route path='/edit-course/:id' element={<EditCourse />} />
+                    <Route path='/courses' element={<Courses />} />
+                </Routes>
+            </Router>
+        </Provider>
+    )
+}
+
+export default NavRoutes;

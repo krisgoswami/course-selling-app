d1c0d641833200fd7209dfba7d71c4cb59468300
Krishanu <krishgoswami15@gmail.com>
2023-10-01 13:02:11 +0530

    made changes to routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -113,6 +113,26 @@ export const getCourses = async (req, res) => {
     }
 }
 
+// *********get course by id*********
+
+export const getCourse = async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        const course = await Course.findById(courseId);
+        res.status(200).send({
+            success: true,
+            course,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            message: 'No course found',
+            success: false,
+            error,
+        });
+    }
+}
+
 // *********purchase course*********
 
 export const purchaseCourse = async (req, res) => {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { authenticateJwt } from "../utils/jwtAuth.js";
-import { createUser, userLogin, getCourses, purchaseCourse, purchasedCourses } from '../controllers/userController.js';
+import { createUser, userLogin, getCourses, purchaseCourse, purchasedCourses, getCourse } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signup', createUser);
 router.post('/login', userLogin);
 router.get('/courses', getCourses);
+router.get('/course/:id', getCourse);
 router.post('/purchaseCourse/:id', authenticateJwt, purchaseCourse);
 router.get('/purchasedCourses', authenticateJwt, purchasedCourses);
 

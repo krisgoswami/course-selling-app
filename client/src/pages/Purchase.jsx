857ef36daa216023068b1ec1dce15ce11e857fd3
Krishanu <krishgoswami15@gmail.com>
2023-10-02 01:24:23 +0530

    added purchase page

---
+++ client/src/pages/Purchase.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { Box, Input, Button, Text } from '@chakra-ui/react';
+import toast from 'react-hot-toast';
+import { BASE_URL } from '../utils/helper';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const Purchase = () => {
+    const [cardNumber, setCardNumber] = useState('');
+    const id = useParams().id;
+    const token = localStorage.getItem('token');
+    const email = localStorage.getItem('email');
+    const navigate = useNavigate();
+
+    const handlePurchase = async () => {
+        try {
+            if (cardNumber === '1234-5678-1234-5678') {
+                const { data } = await axios.post(`${BASE_URL}/api/v1/user/purchaseCourse/${id}`, {
+                    email: email,
+                }, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                });
+                console.log(data);
+                if (data.success) {
+                    toast.success("Course Purchased");
+                    navigate('/purchased-courses');
+                }
+            } else {
+                toast.error('Invalid card number');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    return (
+        <>
+            <Box maxW="400px" mx="auto" p={4}>
+                <h1>Purchase Course</h1>
+                <Input
+                    type="text"
+                    value={cardNumber}
+                    onChange={(e) => setCardNumber(e.target.value)}
+                    placeholder="Enter Credit Card Number"
+                    mb={4}
+                />
+                <Button colorScheme="blue" onClick={handlePurchase} mb={4}>
+                    Purchase
+                </Button>
+            </Box>
+        </>
+    );
+};
+
+export default Purchase;
+
+
+
+
+
